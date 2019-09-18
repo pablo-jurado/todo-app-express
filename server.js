ffad67a40704bb4d2d855e11981b5d7e1f15eb9c
pandafulmanda <pandafulmanda@gmail.com>
2019-09-17 20:52:00 -0500

    add comments with more details

--- server.js
+++ server.js
@@ -13,14 +13,32 @@ var todoList = [
 ];
 
 // GET /api/todos
+// This should respond with the full list of todo items.
 
 // GET /api/todos/:id
+// This should respond with the information for the matching todo item
+// by id.
+// If the matching todo does not exist, the server should respond
+// with a 404 status code.
 
 // POST /api/todos
+// This should take the body of the request and add it to todoList.
+// Remember to generate a unique id for the new todo item.
+// This endpoint should respond with the new item with it's id.
 
 // PUT /api/todos/:id
+// This should update the matching todo item by id with the
+// body of the request.  The endpoint should respond with the
+// updated item.
+// If the matching todo does not exist, the server should respond
+// with a 404 status code.
 
 // DELETE /api/todos/:id
+// This should remove the matching item from the list of todo items.
+// This endpoint should respond with the new length of the list.
+// If the matching todo does not exist, the server should respond
+// with a 404 status code.
+
 
 app.listen(3000, function(){
     console.log('Todo List API is now listening on port 3000...');
